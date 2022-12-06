a988af756aa7b511f7707027746c1f5396090c19
hilmanystrom <nyhi22bc@student.ju.se>
2022-12-06 16:07:33 +0100

    Final project work with group

---
+++ Final project/main.js
@@ -0,0 +1,18 @@
+
+// let modal = document.getElementById("myModal");
+// let button = document.getElementById("myButton");
+// let span = document.getElementById("close")[0];
+
+// button.onclick = function() {
+//     modal.style.display = "block";
+// }
+
+// span.onclick = function() {
+//     modal.style.display = "none";
+// }
+
+// window.onclick = function() {
+//     if (Event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
